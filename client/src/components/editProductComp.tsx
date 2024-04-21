e9c9cf7b549ce822f591b2579a4aaa82b487a53a
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-04-21 12:10:28 +0200

    edit product endpoint & feature working as intended

--- client/src/components/editProductComp.tsx
+++ client/src/components/editProductComp.tsx
@@ -14,6 +14,8 @@ import {
 import { IProduct } from "@/models/interfaces/products"
 import axios from "axios"
 import { toast } from "./ui/use-toast"
+import { useState } from "react"
+import { NewProductDetails } from "@/models/classes/products"
 
 interface IEditProductProps {
   product: IProduct,
@@ -21,6 +23,17 @@ interface IEditProductProps {
 }
 
 export function EditProduct({ product, fetchAllProducts }: IEditProductProps) {
+  const [newProduct, setNewProduct] = useState(new NewProductDetails( "", 0, "", "", "", 0, ""))
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewProduct({
+      ...newProduct,
+      [e.target.name]: e.target.value
+    }
+    )
+    console.log(newProduct)
+
+  }
 
   const handleDelete = async () => {
     try {
@@ -39,8 +52,9 @@ export function EditProduct({ product, fetchAllProducts }: IEditProductProps) {
           description: `Id number ${product._id} cannot be found`,
         })
         fetchAllProducts()
-    
-    }} catch (error) {
+
+      }
+    } catch (error) {
       console.log(error)
       toast({
         variant: "destructive",
@@ -50,7 +64,34 @@ export function EditProduct({ product, fetchAllProducts }: IEditProductProps) {
     }
   }
 
-//TODO sort editing functions for product editing
+  const handleEdit = async () => {
+    try {
+      const res = await axios.put(`http://localhost:3000/products/update/${product._id}`, newProduct )
+      console.log("res from edit", res.data)
+      if (res.status === 200) {
+        toast({
+          title: "Product edited",
+          description: `Id number ${product._id} has been edited`,
+        })
+        fetchAllProducts()
+      } else if (res.status === 404) {
+        toast({
+          variant: "destructive",
+          title: "Product cannot be edited",
+          description: `Id number ${product._id} cannot be found`,
+        })
+        fetchAllProducts()
+
+      }
+    } catch (error) {
+      console.log(error)
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: `There was a problem with your product edit ${error}`,
+      })
+    }
+  }
 
 
 
@@ -77,48 +118,48 @@ export function EditProduct({ product, fetchAllProducts }: IEditProductProps) {
             <Label htmlFor="status" className="text-right">
               Status
             </Label>
-            <Input id="status" placeholder={product.status} className="col-span-3" />
+            <Input name={"status"} onChange={handleChange} id="status" placeholder={product.status} className="col-span-3" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="name" className="text-right">
               Name
             </Label>
-            <Input id="name" placeholder={product.name} className="col-span-3" />
+            <Input name={"name"} onChange={handleChange} id="name" placeholder={product.name} className="col-span-3" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="description" className="text-right">
               Description
             </Label>
-            <Input id="description" placeholder={product.description} className="col-span-3" />
+            <Input name={"description"} onChange={handleChange} id="description" placeholder={product.description} className="col-span-3" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="image" className="text-right">
               Image URL
             </Label>
-            <Input id="image" placeholder={product.image} className="col-span-3" />
+            <Input name={"image"} onChange={handleChange} id="image" placeholder={product.image} className="col-span-3" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="price" className="text-right">
               Price
             </Label>
-            <Input id="price" placeholder={product.price.toString()} className="col-span-3" />
+            <Input name={"price"} onChange={handleChange} id="price" className="col-span-3" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="category" className="text-right">
               Category
             </Label>
-            <Input id="category" placeholder={product.category} className="col-span-3" />
+            <Input name={"category"} onChange={handleChange} id="category" placeholder={product.category} className="col-span-3" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="amountInStock" className="text-right">
               Num. in Stock
             </Label>
-            <Input id="amountInStock" placeholder={product.amountInStock.toString()} className="col-span-3" />
+            <Input name={"amountInStock"} onChange={handleChange} id="amountInStock" className="col-span-3" />
           </div>
         </div>
         <SheetFooter>
           <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
+            <Button onClick={handleEdit} type="submit">Save changes</Button>
           </SheetClose>
           <Button onClick={handleDelete} variant="destructive">Delete product</Button>
         </SheetFooter>
